b55851dc08b4c7b5e21cfb0500a7fc84f79ce522
codew45 <106466457+codew45@users.noreply.github.com>
2022-05-29 21:14:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 1680 ms, Memory: 56800 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[][] str = new String[N][2];
+        
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            str[i][0] = st.nextToken();
+            str[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(str, (o1, o2)->{
+            return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+        });
+        
+        for(int i=0;i<N;i++){
+            System.out.println(str[i][0]+" "+str[i][1]);
+        }
+    }    
+}
