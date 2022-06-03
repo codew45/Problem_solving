9d04243551b7c54d001508171c3ede9ab571c575
codew45 <106466457+codew45@users.noreply.github.com>
2022-06-03 18:49:10 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        String star = "*";
+        String space = " ";
+        
+        for(int i=1;i<=N;i++) {
+            sb.append(space.repeat(N-i)).append(star.repeat(2*i-1)).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
