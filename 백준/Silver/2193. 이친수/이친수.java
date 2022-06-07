ccdf0fd051c9881e18b7755b0ec5b9cd4e479381
codew45 <106466457+codew45@users.noreply.github.com>
2022-06-07 20:48:46 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -6,14 +6,24 @@ public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] dp = new long[N+1];
+        long[][] dp = new long[N+1][2];
         
-        dp[0] = 0;
-        dp[1] = 1;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
         
         for(int i=2;i<=N;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            for(int j=0;j<2;j++){
+                if(j==0){
+                    dp[i][j] = dp[i-1][0]+dp[i-1][1];
+                } 
+                else{
+                    dp[i][j] = dp[i-1][0];
+                } 
+            }
         }
-        System.out.println(dp[N]);
+        long sum = 0;
+        for(int i=0;i<2;i++) sum += dp[N][i];
+        
+        System.out.println(sum);
     }
 }
