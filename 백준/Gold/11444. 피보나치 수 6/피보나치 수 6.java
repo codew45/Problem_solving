8a697c9de248bfc65aee7b115ad690cfc4020d34
codew45 <106466457+codew45@users.noreply.github.com>
2022-09-18 15:32:01 +0900

    [Gold II] Title: 피보나치 수 6, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	final static long MOD = 1000000007;
+	static long[][] fibo = {{1,1},{1,0}};
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		long N = Long.parseLong(br.readLine());
+		
+		System.out.println(findFibo(fibo, N-1)[0][0]);
+	}
+	
+	static long[][] findFibo(long[][] arr, long n) {
+		
+		if(n<2) {
+			return arr;
+		}
+		
+		long[][] tmp = findFibo(arr,n/2);
+		
+		tmp = multiply(tmp, tmp);
+		
+		if(n%2==1) {
+			tmp = multiply(tmp, fibo);
+		}
+		
+		return tmp;
+	}
+	static long[][] multiply(long[][] a, long[][] b){
+		long[][] tmp = new long[2][2];
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<2;j++) {
+				for(int k=0;k<2;k++) {
+					tmp[i][j] += a[i][k]*b[k][j];
+					tmp[i][j] %= MOD;
+				}
+			}
+		}
+		return tmp;
+	}
+}
