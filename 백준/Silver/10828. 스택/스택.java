0962bf5966dd058f3ebfcf748c44f523ff6a4b84
codew45 <106466457+codew45@users.noreply.github.com>
2022-09-19 17:17:26 +0900

    [Silver IV] Title: 스택, Time: 188 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,67 +2,80 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static ArrayList<Integer> stack = new ArrayList<Integer>();
+	static int[] stack;
+	static int size = 0;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 		
+		stack = new int[N];
+		
+		StringBuilder sb = new StringBuilder();
+		
 		for(int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			
-			if(st.countTokens()==2) {
-				st.nextToken();
+			switch(st.nextToken()) {
+			
+			case "push":
 				push(Integer.parseInt(st.nextToken()));
-			}
-			else {
-				String command = st.nextToken();
+				break;
 				
-				if(command.equals("pop")) pop();
-				else if(command.equals("size")) size();
-				else if(command.equals("empty")) empty();
-				else if(command.equals("top")) top();
+			case "pop":
+				sb.append(pop()).append('\n');
+				break;
+				
+			case "size":
+				sb.append(size()).append('\n');
+				break;
+				
+			case "empty":
+				sb.append(empty()).append('\n');
+				break;
+				
+			case "top":
+				sb.append(top()).append('\n');
+				break;
 			}
 			
 		}
+		System.out.println(sb);
 	}
 	
 	static void push(int x) {
-		stack.add(x);
+		stack[size] = x;
+		size++;
 		return;
 	}
 	
-	static void pop() {
-		if(stack.isEmpty()) {
-			System.out.println(-1);
-			return;
+	static int pop() {
+		if(size==0) {
+			return -1;
 		}
-		System.out.println(stack.get(stack.size()-1));
-		stack.remove(stack.size()-1);
-		return;
+		
+		int tmp = stack[size-1];
+		stack[size-1] = 0;
+		size--;
+		return tmp;
 	}
 	
-	static void size() {
-		System.out.println(stack.size());
-		return;
+	static int size() {
+		return size;
 	}
 	
-	static void empty() {
-		if(stack.isEmpty()) {
-			System.out.println(1);
-			return;
+	static int empty() {
+		if(size==0) {
+			return 1;
 		}
-		System.out.println(0);
-		return;
+		return 0;
 	}
 	
-	static void top() {
-		if(stack.isEmpty()) {
-			System.out.println(-1);
-			return;
+	static int top() {
+		if(empty()==1) {
+			return -1;
 		}
-		System.out.println(stack.get(stack.size()-1));
-		return;
+		return stack[size-1];
 	}
 }
