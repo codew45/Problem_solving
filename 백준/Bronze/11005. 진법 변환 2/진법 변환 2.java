d2d464305fe1845ce74e3e2c5052bd7b5b92ab15
codew45 <106466457+codew45@users.noreply.github.com>
2022-09-30 22:11:42 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int res = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(N>0) {
+			
+			if((N%B)<10) {
+				sb.append((char)(N%B+'0'));
+			}
+			else {
+				sb.append((char)(((N%B)-10)+'A'));
+			}
+			N /= B;
+		}
+		System.out.println(sb.reverse());
+	}
+
+}
