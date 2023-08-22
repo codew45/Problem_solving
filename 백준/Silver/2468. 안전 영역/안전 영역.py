08523c72502952f5593fbe8a7c8c597c3c0a323c
codew45 <106466457+codew45@users.noreply.github.com>
2023-08-22 18:25:39 +0900

    [Silver I] Title: 안전 영역, Time: 788 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -3,27 +3,30 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-graph = [[]for i in range(n)]
+graph = []
 
 for i in range(n):
-    graph[i] = list(map(int,input().split()))
+    graph.append(list(map(int,input().split())))
 
 max_height = max(map(max,graph))
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-def bfs(x,y,h):
+def bfs(graph,visited,x,y,h):
     queue = deque()
     queue.append((x,y))
     visited[x][y] = True
+
     while queue:
         a,b = queue.popleft()
+
         for i in range(4):
             nx = a+dx[i]
             ny = b+dy[i]
+
             if nx>=0 and nx<n and ny>=0 and ny<n:
-                if not visited[nx][ny]:
+                if not visited[nx][ny] and graph[nx][ny]>h:
                     queue.append((nx,ny))
                     visited[nx][ny]=True
 
@@ -33,12 +36,9 @@ for i in range(max_height):
     visited = [[False for j in range(n)]for k in range(n)]
     for j in range(n):
         for k in range(n):
-            if graph[j][k]<=i:
-                visited[j][k] = True
-    for j in range(n):
-        for k in range(n):
-            if not visited[j][k]:
-                bfs(j,k,i)
-                cnt +=1
+            if graph[j][k]>i and visited[j][k]==False:
+                bfs(graph,visited,j,k,i)
+                cnt+=1
+
     res.append(cnt)
 print(max(res))
