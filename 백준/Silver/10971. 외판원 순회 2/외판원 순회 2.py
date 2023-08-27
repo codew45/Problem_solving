59918ec6922a61897c92f4683a3bad7ac702c0f8
codew45 <106466457+codew45@users.noreply.github.com>
2023-08-27 15:46:32 +0900

    [Silver II] Title: 외판원 순회 2, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,30 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(start, now, value, cnt):
+def dfs(depth, start, value):
     global min_value
-    if cnt==n:
-        if graph[now][start]!=0:
-            min_value = min(min_value,value+graph[now][start])
+    if depth == n and graph[start][0]!=0:
+        min_value = min(min_value,value+graph[start][0])
         return
     if value>min_value:
         return
     for i in range(n):
-        if visited[i]==0 and graph[now][i]!=0:
-            visited[i] = 1
-            dfs(start, i, value+graph[now][i],cnt+1)
-            visited[i] = 0
-        
-        
-n=int(input())
-graph = []
-visited = [0]*n
+        if visited[i]==0 and graph[start][i]!=0:
+            visited[i]=1
+            dfs(depth+1,i,value+graph[start][i])
+            visited[i]=0
+
+
+
+n = int(input())
 min_value = sys.maxsize
+graph = []
 for i in range(n):
     graph.append(list(map(int,input().split())))
-    
-for i in range(n):
-    visited[i] = 1
-    dfs(i,i,0,1)
-    visited[i] = 0
+visited = [0]*n
+
+visited[0]=1
+dfs(1,0,0)
 print(min_value)
