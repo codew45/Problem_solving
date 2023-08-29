dd553e9e14baf225d63d1589340bdb49e73938af
codew45 <106466457+codew45@users.noreply.github.com>
2023-08-29 21:32:58 +0900

    [Silver II] Title: 좌표 압축, Time: 2056 ms, Memory: 154808 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,16 +1,11 @@
 n = int(input())
 arr = list(map(int,input().split()))
 s = set(arr)
-sorted_arr = []
-for i in range(len(arr)):
-    sorted_arr.append([i,arr[i]])
-sorted_arr.sort(key=lambda x:(x[1]), reverse=True)
-res = []
-for i in range(len(arr)):
-    if sorted_arr[i][1] in s:
-        s.remove(sorted_arr[i][1])
-    res.append([sorted_arr[i][0], sorted_arr[i][1], len(s)])
-res.sort(key=lambda x:(x[0]))
+s = sorted(s)
+s_list = list(s)
+d = {}
 
-for i in res:
-    print(i[2],end= ' ')
+for i in range(len(s)):
+    d[s_list[i]] = i
+for i in range(len(arr)):
+    print(d[arr[i]],end= ' ')
