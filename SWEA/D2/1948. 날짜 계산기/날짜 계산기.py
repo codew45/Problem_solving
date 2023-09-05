0c8756d6046f955536c7bad521a494b1b256f2c1
codew45 <106466457+codew45@users.noreply.github.com>
2023-09-05 20:02:20 +0900

    [D2] Title: 날짜 계산기, Time: 92 ms, Memory: 42,704 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -4,9 +4,9 @@ days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 for test_case in range(1, T + 1):
     m1, d1, m2, d2 = map(int,input().split())
     total = 1
-    if m2-m1>0:
-        for i in range(1,m2-m1+1):
-            total += days[m2-i]
+    
+    for i in range(m1,m2):
+        total += days[i]
     total = total -d1+d2
     print('#%d'%test_case,'%d'%total)
     
