0f460083c94696bf82bbd3557606bf64ce3287b6
codew45 <106466457+codew45@users.noreply.github.com>
2023-11-07 17:01:18 +0900

    [Gold IV] Title: 공유기 설치, Time: 7672 ms, Memory: 40412 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,27 +1,23 @@
+n,c=map(int,input().split())
 
-n,c= map(int,input().split())
-house = sorted([int(input()) for _ in range(n)])
+data = sorted([int(input()) for _ in range(n)])
 
-house.sort()
-
-start,end = 1, house[-1]-house[0]
-answer = 0
+start = 1
+end = data[-1]-data[0]
+result = 0
 
 while start<=end:
     mid = (start+end)//2
-    current = house[0]
+    current = data[0]
     cnt = 1
-
+    
     for i in range(1,n):
-        if house[i]-current>=mid:
+        if data[i]-current>=mid:
             cnt +=1
-            current = house[i]
-            
+            current = data[i]
     if cnt>=c:
-        if answer<mid:
-            answer = mid
+        result = mid
         start = mid+1
     else:
         end = mid-1
-
-print(answer)
+print(result)
