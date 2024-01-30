2d88c604d97ec448b7f2e007314248217611f1a7
codew45 <106466457+codew45@users.noreply.github.com>
2024-01-30 22:32:11 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 22652 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,44 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int M;
+	static boolean [] isSelected;
+	static StringBuilder sb = new StringBuilder();
+	static int [] numbers;
+	
+	public static void perm(int cnt) {
+		if(cnt==M) {
+			for(int n : numbers) {
+				sb.append(n).append(" ");
+			}
+			sb.append("\n");
+		}
+		else {
+			for(int i=1;i<=N;i++) {
+				if(!isSelected[i]) {
+					numbers[cnt] = i;
+					isSelected[i]=true;
+					perm(cnt+1);
+					isSelected[i] = false;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		isSelected = new boolean[N+1];
+		numbers = new int[M];
+		
+		perm(0);
+		System.out.println(sb);
+		
+	}
+
+}
