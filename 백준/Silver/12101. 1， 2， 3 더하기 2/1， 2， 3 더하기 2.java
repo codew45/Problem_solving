49c4c8356b95c4814df7e51759f641c252212a66
codew45 <106466457+codew45@users.noreply.github.com>
2024-02-25 17:27:25 +0900

    [Silver I] Title: 1, 2, 3 더하기 2, Time: 84 ms, Memory: 11764 KB -BaekjoonHub

--- 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
+++ 백준/Silver/12101. 1， 2， 3 더하기 2/1， 2， 3 더하기 2.java
@@ -1,42 +1,41 @@
 import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str[] = br.readLine().split(" ");
-        int N = Integer.parseInt(str[0]);
-        int K = Integer.parseInt(str[1]);
-        
-        ArrayList<String>[] arr = new ArrayList[N+3];
-        
-        for(int i=0;i<N+3;i++) {
-        	arr[i] = new ArrayList<>();
-        }
-        
-        arr[1].add("1");
-        arr[2].add("1+1");
-        arr[2].add("2");
-        arr[3].add("1+1+1");
-        arr[3].add("1+2");
-        arr[3].add("2+1");
-        arr[3].add("3");
-        
-        for(int i=4;i<=N;i++) {
-        	for(int j=1;j<=3;j++) {
-        		for(String op : arr[i-j]) {
-        			arr[i].add(op+"+"+j);
-        		}
-        	}
-        }
-        
-        if(arr[N].size()<K) {
-        	System.out.println(-1);
-        }
-        else {
-			Collections.sort(arr[N]);
-        	System.out.println(arr[N].get(K-1));
-        }
-        
-    }
-    
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		ArrayList<ArrayList<String>> list = new ArrayList<>();
+		
+		for(int i=0;i<n+3;i++) list.add(new ArrayList<>());
+		
+		list.get(1).add("1");
+		list.get(2).add("1+1");
+		list.get(2).add("2");
+		list.get(3).add("1+1+1");
+		list.get(3).add("1+2");
+		list.get(3).add("2+1");
+		list.get(3).add("3");
+		
+		for(int i=4;i<=n;i++) {
+			for(int j=1;j<=3;j++) {
+				for(String op:list.get(i-j)) {
+					list.get(i).add(op+"+"+j);
+				}
+			}
+		}
+		
+		if(list.get(n).size()<k) System.out.println(-1);
+		else {
+			Collections.sort(list.get(n));
+			System.out.println(list.get(n).get(k-1));
+		}
+		
+		
+	}
+
 }
