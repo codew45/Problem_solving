e567bc231672cc76e2c2beed088e494e729d1745
codew45 <106466457+codew45@users.noreply.github.com>
2024-03-18 20:14:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,29 +1,40 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
-        int[] dp = new int[N+1];
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        
-        for(int i=1;i<=N;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        dp[1] = 1;
-        
-        for(int i=2;i<=N;i++){
-            dp[i] = 1;
-            for(int j=0;j<i;j++){
-                if(arr[i]>arr[j]&&dp[i]<=dp[j]+1) dp[i] = dp[j]+1;
-            }
-        }
-        int max = 0;
-        for(int val : dp) if(val>max) max = val;
-        System.out.println(max);
-    }
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] input = new int[n];
+		int[] dp = new int[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0] = 1;
+		
+		for(int i=1;i<n;i++) {
+			dp[i] = 1;
+			for(int j=0;j<i;j++) {
+				// j번쨰 수보다 i번째 수가 크고 
+				// j번쨰까지의 최장 증가 수열에 i번째수를 포함한 게 더 길다면
+				// i번째까지 포함시킨다
+				if(input[i]>input[j]&&dp[i]<=dp[j]+1) {
+					dp[i] = dp[j]+1;
+				}
+			}
+		}
+		
+		int maxLen = 0;
+		for(int i:dp) {
+			maxLen = Math.max(maxLen, i);
+		}
+		System.out.println(maxLen);
+	}
+
 }
