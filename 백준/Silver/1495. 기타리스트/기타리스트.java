06db0f50335c3068dee646e63939f9ac027ec1f4
codew45 <106466457+codew45@users.noreply.github.com>
2024-03-27 22:05:23 +0900

    [Silver I] Title: 기타리스트, Time: 124 ms, Memory: 15088 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -19,16 +19,12 @@ public class Main {
 			v[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// dp의 인덱스 : 곡의 번호
-		// dp.get(idx) set에 들어있는 데이터들 : idx번째 곡을 연주하는 데에 사용할 수 있는 볼륨들
 		ArrayList<Set<Integer>> dp = new ArrayList<>();
 		
-		// 곡의 개수만큼 리스트를 만들어줌
 		for(int i=0;i<=n;i++){
 			dp.add(new HashSet<>());
 		}
 		
-		// 시작 볼륨 s를 넣고 시작
 		Set<Integer> set = new HashSet<>();
 		set.add(s);
 		dp.get(0).addAll(set);
