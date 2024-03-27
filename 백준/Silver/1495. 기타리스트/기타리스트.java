eb65bd2021cfd95b5d5eb711b74db5f9a1c6eb2d
codew45 <106466457+codew45@users.noreply.github.com>
2024-03-27 22:04:21 +0900

    [Silver I] Title: 기타리스트, Time: 128 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.java
+++ 백준/Silver/1495. 기타리스트/기타리스트.java
@@ -1,10 +1,8 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
 	static int n, s, m;
 	static int[] v;
-	static int max = -1;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,15 +19,20 @@ public class Main {
 			v[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		// dp의 인덱스 : 곡의 번호
+		// dp.get(idx) set에 들어있는 데이터들 : idx번째 곡을 연주하는 데에 사용할 수 있는 볼륨들
 		ArrayList<Set<Integer>> dp = new ArrayList<>();
 		
+		// 곡의 개수만큼 리스트를 만들어줌
 		for(int i=0;i<=n;i++){
 			dp.add(new HashSet<>());
 		}
 		
+		// 시작 볼륨 s를 넣고 시작
 		Set<Integer> set = new HashSet<>();
 		set.add(s);
 		dp.get(0).addAll(set);
+		
 		
 		for(int i=0;i<n;i++) {
 			Set<Integer> tmp = new HashSet<>();
@@ -39,34 +42,33 @@ public class Main {
 				int small = data-v[i];
 				
 				if(small>=0) {
-					tmp.add(small);
+					if(!tmp.contains(small)) {
+						tmp.add(small);
+					}
 				}
+				
 				if(big<=m) {
-					tmp.add(big);
+					if(!tmp.contains(big)) {
+						tmp.add(big);
+					}
 				}
 			}
 			
 			dp.get(i+1).addAll(tmp);
 		}
 		
-		int max = Integer.MIN_VALUE;
+		int maxVolume = Integer.MIN_VALUE;
 		
 		for(int data:dp.get(n)) {
-			max = Math.max(max, data);
+			maxVolume = Math.max(maxVolume, data);
 		}
 		
-		if(max==Integer.MIN_VALUE) {
+		if(maxVolume == Integer.MIN_VALUE) {
 			System.out.println(-1);
-		}else {
-			System.out.println(max);
 		}
-		
+		else {
+			System.out.println(maxVolume);
+		}
 		
 	}
-
 }
-
-
-
-
-
