abb10c3096cc769cd03eba20dabf5f5c2b893d17
codew45 <106466457+codew45@users.noreply.github.com>
2025-01-01 16:05:52 +0900

    [Gold IV] Title: 문자열 폭발, Time: 420 ms, Memory: 87068 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,44 +1,49 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input = br.readLine();
-		String target = br.readLine();
-		
-		int inputLen = input.length();
-		int targetLen = target.length();
-		
-		Stack<Character> stack = new Stack<>();
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i=0;i<inputLen;i++) {
-			
-			stack.push(input.charAt(i));
-			
-			boolean flag = true;
-			
-			if(stack.size()>=targetLen&&stack.peek()==target.charAt(targetLen-1)) {
-				for(int j=0;j<targetLen;j++) {
-					if(stack.get(stack.size()-j-1)!=target.charAt(targetLen-j-1)) {
-						flag = false;
-					}
-				}
-				if(flag) {
-					for(int j=0;j<targetLen;j++) stack.pop();
-				}
-			}
-			
-		}
-		if(stack.isEmpty()) {
-			System.out.println("FRULA");
-		}
-		else {
-			while(!stack.isEmpty()) sb.append(stack.pop());
-			System.out.println(sb.reverse());
-		}
-		
-	}
-
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    String input = br.readLine();
+    String target = br.readLine();
+    
+    int inputLen = input.length();
+    int targetLen = target.length();
+    
+    Stack<Character> stack = new Stack<>();
+    
+    for(int i=0; i<inputLen ;i++){
+        
+        char c = input.charAt(i);
+        
+        stack.push(c);
+        boolean flag = true;
+        
+        if(stack.size() >= targetLen && stack.peek() == target.charAt(targetLen-1)){
+            for(int j=0; j<targetLen ;j++){
+                
+                if(stack.get( stack.size()-1-j ) != target.charAt(targetLen-1-j)){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag){
+                for(int j=0;j<targetLen;j++) stack.pop();
+            }
+        }
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    
+    if(stack.isEmpty()){
+        System.out.println("FRULA");
+    }else{
+        while(!stack.isEmpty()) sb.append(stack.pop());
+        System.out.println(sb.reverse());
+    }
+    
+    
+  }
 }
