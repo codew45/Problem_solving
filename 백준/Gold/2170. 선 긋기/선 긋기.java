4a2780b20ab1cf911f9f582ac79ad5b59dac92c6
codew45 <106466457+codew45@users.noreply.github.com>
2025-01-18 16:54:53 +0900

    [Gold V] Title: 선 긋기, Time: 1232 ms, Memory: 324000 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -2,78 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static class Line{
-		int start;
-		int end;
-		public Line(int start, int end) {
-			super();
-			this.start = start;
-			this.end = end;
-		}
-		
-		@Override
-		public String toString() {
-			return "Line [start=" + start + ", end=" + end + "]";
-		}
-		
-	}
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Line> lines = new PriorityQueue<>(new Comparator<Line>() {
-			@Override
-			public int compare(Line o1,Line o2) {
-				if(o1.end!=o2.end) {
-					return o2.end-o1.end;
-				}
-				else {
-					return o2.start-o1.start;
-				}
-			}
-		});
-		
-		for(int i=0;i<n;i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			
-			lines.offer(new Line(start, end));
-		}
-		
-		Line line = lines.poll();
-		int prevStart = line.start;
-		int prevEnd = line.end;
-		int len = line.end-line.start;
-		
-		while(!lines.isEmpty()) {
-			Line next = lines.poll();
-			int nextStart = next.start;
-			int nextEnd = next.end;
-//			System.out.println(next.toString());
-			
-			if(prevStart>nextEnd) {
-				len += nextEnd - nextStart;
-//				System.out.println(nextEnd - nextStart+" 추가");
-			}
-			else {
-				if(prevStart>nextStart) {
-					len += prevStart-nextStart;
-//					System.out.println( prevStart-nextStart+" 추기");
-				}
-			}
-			
-			if(prevStart>nextStart) {
-				prevStart = nextStart;
-			}
-			if(prevEnd<nextEnd) {
-				prevEnd = nextEnd;
-			}
-//			System.out.println("LEN "+len);
-		}
-		System.out.println(len);
-
-	}
-
+    static class Line implements Comparable<Line>{
+        int start;
+        int end;
+        public Line(int start, int end){
+            this.start = start;
+            this.end = end;
+        }
+        
+        @Override
+        public int compareTo(Line o){
+            if(this.end == o.end) return this.end - o.end;
+            return this.start - o.start;
+        }
+    }
+    
+  public static void main(String args[]) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int N = Integer.parseInt(br.readLine());
+      
+      PriorityQueue<Line> pq = new PriorityQueue<Line>();
+      
+      for(int i=0;i<N;i++){
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          
+          int s = Integer.parseInt(st.nextToken());
+          int e = Integer.parseInt(st.nextToken());
+          
+          pq.add(new Line(s,e));
+      }
+      
+      Line first = pq.poll();
+      int len = first.end - first.start;
+      int s = first.start;
+      int e = first.end;
+      
+      while(!pq.isEmpty()){
+          Line next = pq.poll();
+          
+          int ns = next.start;
+          int ne = next.end;
+          
+          if(e < ns){
+              len += ne - ns;
+              s = ns;
+              e = ne;
+          }
+          else if(s <= ns){
+              if(e < ne){
+                len += ne - e;
+                e = ne;
+              }
+          }
+      }
+      
+      System.out.println(len);
+      
+  }
 }
