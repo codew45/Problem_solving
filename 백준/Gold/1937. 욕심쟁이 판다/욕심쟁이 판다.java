30d80008045e5b9dd5c6582fab9d4c9a993b1656
codew45 <106466457+codew45@users.noreply.github.com>
2025-02-16 15:47:13 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 380 ms, Memory: 40680 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    
+    static int N;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static int maxStep = Integer.MIN_VALUE;
+    
+    static int dfs(int x, int y){
+        
+        if(dp[x][y] != 0) return dp[x][y];
+        
+        dp[x][y] = 1;
+        
+        for(int d=0; d<4; d++){
+            int nx = x+dx[d];
+            int ny = y+dy[d];
+            
+            if(nx >= 0 && nx<N && ny>=0 && ny<N && map[x][y] < map[nx][ny]){
+                
+                dp[x][y] = Math.max(dp[x][y], dfs(nx,ny)+1);
+            }
+            
+        }
+        
+        return dp[x][y];
+    }
+    
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        dp = new int[N][N];
+        
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                maxStep = Math.max(maxStep,dfs(i, j));
+            }
+        }
+        
+        System.out.println(maxStep);
+    }
+}
