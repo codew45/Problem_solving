42a892e272c3dc3de4ba7ad1531039616d78b5e8
codew45 <106466457+codew45@users.noreply.github.com>
2025-02-18 15:59:21 +0900

    [level 2] Title: 점 찍기, Time: 11.66 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -2,35 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    
-    static int binarySearch(int x, int k, int d){
-        int start = 0;
-        int end = d;
-        int maxJ = 0;
-        
-        long xSquare = (long) Math.pow(x, 2);
-        long dSquare = (long) Math.pow(d, 2);
-        
-        while(start <= end){
-            int mid = (start+end)/2;
-            
-            if( xSquare + Math.pow( mid*k, 2) <= dSquare ){
-                maxJ = Math.max(maxJ, mid);
-                
-                start = mid+1;
-            }else{
-                end = mid-1;
-            }
-        }
-        
-        return maxJ;
-    }
-    
     public long solution(int k, int d) {
         long answer = 0;
+        long dSquare = (long) d*d;
         
         for(int x=0; x<=d; x+=k ){
-            answer += binarySearch(x, k, d) + 1;
+            long xSquare = (long)x*x;
+            int maxY = (int) Math.sqrt( dSquare - xSquare )/k;
+            
+            answer += maxY+1;
+            
         }
         
         return answer;
